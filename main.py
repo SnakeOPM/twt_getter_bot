be35bb7e26fbceacb924fd6dbdea8c79f22346e5
SnakeOPM <snikersdaun@gmail.com>
2023-01-13 20:54:02 +1000

    Made this more extendable for upcoming features

--- main.py
+++ main.py
@@ -17,26 +17,27 @@ bot = telebot.TeleBot(token)
 def start_messedge(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     login = types.KeyboardButton("Свиня")
-    keyboard.add(login)
+    hash = types.KeyboardButton('Поиск по #')
+    keyboard.add(login, hash)
     bot.send_message(message.chat.id,'Привет, я подростающий, пришлите мне тег для поиска', reply_markup=keyboard)
 
 
-@bot.message_handler(func=lambda message:True)
+@bot.message_handler(func=lambda msg: msg.text == "Свиня")
 def reply_on_button(messege):
     filename = 'https://tenor.com/ru/view/pig-azamaz-gif-24202539'
-    if messege.text == "Свиня":
-        bot.send_animation(messege.chat.id, filename)
-    else:
-        get_tweet_by_hash(messege)
+    bot.send_animation(messege.chat.id, filename)
 
 def get_tweet_by_hash(messege):
+    if messege.text[0] != '#':
+        bot.send_message(messege.chat.id, 'Неправильный набор')
+        return False
     bot.send_message(messege.chat.id, 'начинаем поиск')
 
     finder = Find_info(messege.text)
     result = finder.find_by_hash()
     i = 0
     for item in result:
-        bot.send_message(messege.chat.id, f'Автор {result["username"][i]}\n пост {result["content"][i]}')
+        bot.send_message(messege.chat.id, f'<b>Автор:</b> <code>{result["username"][i]}</code>\n <b></b> <code>{result["content"][i]}</code>\n', parse_mode='HTML')
         try:
             bot.send_message(messege.chat.id, result['full'][i])
         except Exception as e:
@@ -44,10 +45,10 @@ def get_tweet_by_hash(messege):
         i+=1
 
 
-
+@bot.message_handler(func=lambda msg: msg.text == 'Поиск по #' and msg.content_type == 'text')
 def redirect_to_hash(messege):
-    msg = bot.reply_to(messege, 'Введите тег (Без #)')
-    bot.register_callback_query_handler(msg, get_tweet_by_hash)
+    msg = bot.reply_to(messege, 'Введите тег (С # обязательно)')
+    bot.register_next_step_handler(msg, get_tweet_by_hash)
 
 
 if __name__ == "__main__":
